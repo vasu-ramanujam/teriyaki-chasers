fb347c605d34f66f518edb7940379ab0f913d459
Vasu Ramanujam <vasur@umich.edu>
2025-10-06 16:36:25 -0400

    added navigation bar to content view
    ContentView() now contains a nav bar that shows all five tabs. The Views themselves haven't been called -- I
    commented out where to add calls to each view page within the tab view stack. Each tab currently has a temp icon in addition to text (we can remove this)
    
    tbd: change center (+) to be bigger / more salient

--- ios/wildlifeFinder/wildlifeFinder/Views/ContentView.swift
+++ ios/wildlifeFinder/wildlifeFinder/Views/ContentView.swift
@@ -10,10 +10,48 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            
+            // added temp UI icons. tbd: figure out how to make the + pop out like in the UI mockups
+            TabView{
+                VStack{
+                    //SightingMapView()
+                }
+                .tabItem {
+                    Image(systemName: "map")
+                    Text("Sighting Map")
+                }
+                
+                VStack{
+                    //wherever route mapmode/armode is
+                }
+                .tabItem{
+                    Image(systemName: "point.bottomleft.forward.to.arrow.triangle.scurvepath.fill")
+                    Text("Current Route")
+                }
+                VStack{
+                    // sight a wildlife
+                }
+                .tabItem{
+                    Image(systemName: "plus")
+                        .font(.system(size: 25))
+                }
+                
+                VStack{
+                    // Animal Search view
+                }
+                .tabItem{
+                    Image(systemName: "magnifyingglass")
+                    Text("Animal Search")
+                }
+                VStack{
+                    // User Dashboard view
+                }
+                .tabItem{
+                    Image(systemName: "house.fill")
+                    Text("User Dashboard")
+                }
+                 
+            }
         }
         .padding()
     }
