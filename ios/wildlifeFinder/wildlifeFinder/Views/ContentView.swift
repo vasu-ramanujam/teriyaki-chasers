eec7d964a023b2efe484d2156a87fe4adaf394bb
Vasu Ramanujam <vasur@umich.edu>
2025-10-08 23:33:41 -0400

    added green nav bar

--- ios/wildlifeFinder/wildlifeFinder/Views/ContentView.swift
+++ ios/wildlifeFinder/wildlifeFinder/Views/ContentView.swift
@@ -8,52 +8,35 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State private var selectedTab = 0
+    
     var body: some View {
-        VStack {
+        TabView(selection: $selectedTab) {
+            Tab(value: 0) {
+                // SightingMapView()
+            }
             
-            // added temp UI icons. tbd: figure out how to make the + pop out like in the UI mockups
-            TabView{
-                VStack{
-                    //SightingMapView()
-                }
-                .tabItem {
-                    Image(systemName: "map")
-                    Text("Sighting Map")
-                }
-                
-                VStack{
-                    //wherever route mapmode/armode is
-                }
-                .tabItem{
-                    Image(systemName: "point.bottomleft.forward.to.arrow.triangle.scurvepath.fill")
-                    Text("Current Route")
-                }
-                VStack{
-                    // sight a wildlife
-                }
-                .tabItem{
-                    Image(systemName: "plus")
-                        .font(.system(size: 25))
-                }
-                
-                VStack{
-                    // Animal Search view
-                }
-                .tabItem{
-                    Image(systemName: "magnifyingglass")
-                    Text("Animal Search")
-                }
-                VStack{
-                    // User Dashboard view
-                }
-                .tabItem{
-                    Image(systemName: "house.fill")
-                    Text("User Dashboard")
-                }
-                 
+            Tab(value: 1) {
+                // route view or wtv
+            }
+            
+            Tab(value: 2) {
+                // go to choose add pic / sound
+            }
+            
+            Tab(value: 3) {
+                // animal search
+            }
+            
+            Tab(value: 4) {
+                // user dashboard
             }
         }
-        .padding()
+        .tabViewStyle(PageTabViewStyle(indexDisplayMode: .never))
+        .overlay(alignment: .bottom){
+            BottomTabBarView(selectedTab: $selectedTab)
+        }.ignoresSafeArea()
     }
 }
 
