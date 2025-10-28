195136342a9129df38f741638fbb3cf2816bdd4e
Owen Davis <davisow@umich.edu>
2025-10-28 15:12:41 -0400

    Remove view design with small buttons

--- ios/wildlifeFinder/wildlifeFinder/Views/PostFlowView.swift
+++ ios/wildlifeFinder/wildlifeFinder/Views/PostFlowView.swift
@@ -9,78 +9,29 @@ import SwiftUI
 struct PostFlowView: View {
     var body: some View {
         NavigationStack {
-            InitialViewBigButtons()
+            InitialView()
         }
     }
 }
 
 struct InitialView: View {
-    @State var hasImage = false
-    @State var hasSound = false
-
+    @State var image: UIImage? = nil
+    @State var audioURL: URL? = nil
+    
     var body: some View {
         VStack {
             // Display image
-            if hasImage {
+            if let image = image {
                 Text("Image goes here")
             }
             
             // Add an image
             Button {
                 // Hook to taking the picture
-                hasImage = true // MOVE THIS TO THE CAMERA PAGE WHEN THAT IS IMPLEMENTED
-            } label: {
-                Image(systemName: "camera")
-                Text(hasImage ? "Retake Photo" : "Take Photo")
-            }
-            .padding(.bottom)
-            
-            // Display image
-            if hasSound {
-                Text("Sound goes here")
-            }
-            
-            // Add a sound
-            Button {
-                // Hook to recording sound
-                hasSound = true // MOVE THIS TO THE CAMERA PAGE WHEN THAT IS IMPLEMENTED
-            } label: {
-                Image(systemName: "mic")
-                Text(hasSound ? "Rerecord Audio" : "Record Audio")
-            }
-            .padding(.bottom)
-
-            // Identify the sighting
-            Button {
-                // Hook to Identify page
-            } label: {
-                Text("Identify")
-            }
-            .disabled(!(hasImage || hasSound))
-            .padding(.bottom)
-        }
-    }
-}
-
-struct InitialViewBigButtons: View {
-    @State var hasImage = false
-    @State var hasSound = false
-
-    var body: some View {
-        VStack {
-            // Display image
-            if hasImage {
-                Text("Image goes here")
-            }
-            
-            // Add an image
-            Button {
-                // Hook to taking the picture
-                hasImage = true // MOVE THIS TO THE CAMERA PAGE WHEN THAT IS IMPLEMENTED
             } label: {
                 Image(systemName: "camera")
                     .foregroundStyle(.white)
-                Text(hasImage ? "Retake Photo" : "Take Photo")
+                Text(image != nil ? "Retake Photo" : "Take Photo")
                     .foregroundStyle(.white)
             }
             .padding()
@@ -91,18 +42,17 @@ struct InitialViewBigButtons: View {
             .padding(.bottom)
 
             // Display image
-            if hasSound {
+            if let sound = audioURL {
                 Text("Sound goes here")
             }
             
             // Add a sound
             Button {
                 // Hook to recording sound
-                hasSound = true // MOVE THIS TO THE CAMERA PAGE WHEN THAT IS IMPLEMENTED
             } label: {
                 Image(systemName: "mic")
                     .foregroundStyle(.white)
-                Text(hasSound ? "Rerecord Audio" : "Record Audio")
+                Text(audioURL != nil ? "Rerecord Audio" : "Record Audio")
                     .foregroundStyle(.white)
             }
             .padding()
@@ -112,7 +62,7 @@ struct InitialViewBigButtons: View {
             )
             .padding(.bottom)
 
-            if hasSound || hasImage {
+            if image != nil || audioURL != nil {
                 // Identify the sighting
                 Button {
                     // Hook to Identify page
